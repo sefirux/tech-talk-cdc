6cae427007c9f3367ab4aa3a7baab00158f92ab0
sefirux <JonathanNicolasPaz@Outlook.com>
2026-03-15 18:35:41 -0300

    setup infra

---
+++ init.sql
@@ -0,0 +1,5 @@
+create user asset_user with password 'sup3rS3cr3tP4ssw0rd';
+create database assets_db with owner asset_user;
+
+create user portfolio_user with password 'sup3rS3cr3tP4ssw0rd';
+create database portfolio_db with owner portfolio_user;
